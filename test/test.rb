02befdc5751c6aa99f81ba5be1fa8aa50add8e8d
beta_chelsea <beta.chelsea@gmail.com>
2023-03-06 00:38:16 +0900

    refactor test

--- test/test.rb
+++ test/test.rb
@@ -34,18 +34,18 @@ class TestReversi < Minitest::Test
 
   def test_put_stone
     board = build_board('pattern00.txt')
-    assert_equal true, put_stone!(board, 'e3', BLACK_STONE)
+    assert_equal put_stone!(board, 'e3', BLACK_STONE), true
     assert_equal build_board('pattern00_step1.txt'), board
-    assert_equal true, put_stone!(board, 'f5', WHITE_STONE)
+    assert_equal put_stone!(board, 'f5', WHITE_STONE), true
     assert_equal build_board('pattern00_step2.txt'), board
   end
 
-  def test_cannot_put_stone1
+  def test_cannot_put_stone
     board = build_board('pattern01.txt')
     assert_equal put_stone!(board, 'b8', BLACK_STONE), false
   end
 
-  def test_cannot_put_stone2
+  def test_turn
     board = build_board('pattern02.txt')
     assert_equal put_stone!(board, 'b5', BLACK_STONE), true
     assert_equal build_board('pattern02_after.txt'), board
